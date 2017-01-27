265512b47d67bde5b46574d552a6933bb6f88b58
pogpog03 <johnpaul.calibo0906@gmail.com>
2017-01-27 12:29:02 +0800

    catalog

---
+++ app/src/main/java/com/example/randolph/myapplication/itemlist.java
@@ -0,0 +1,26 @@
+package com.example.randolph.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class itemlist extends AppCompatActivity {
+    private GridView gridView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itemlist);
+        gridView = (GridView) findViewById(R.id.gridITem);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new  AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
